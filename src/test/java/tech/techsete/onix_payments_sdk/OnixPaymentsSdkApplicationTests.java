566947444f03a9d128c128f7fd0a7213069682c3
Edson Isaac <edson-isaac@hotmail.com>
2026-08-25 20:43:17 -0300

    Adiciona arquivos iniciais do projeto, incluindo configuração do Maven, classes principais e arquivos de configuração

---
+++ src/test/java/tech/techsete/onix_payments_sdk/OnixPaymentsSdkApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.techsete.onix_payments_sdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnixPaymentsSdkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
